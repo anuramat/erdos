3ab8e3dad35198df4ee9a379ec4d26f603597fdd
Arsen Nuramatov <anuramat@pm.me>
2022-10-04 19:06:49 +0300

    fixed imports AGAIN

--- app/database.py
+++ app/database.py
@@ -3,8 +3,8 @@ from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from .config import settings
 
+from app.config import settings
 
 url = f"postgresql+psycopg2://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}/{settings.POSTGRES_DB}"
 engine = create_engine(url)
