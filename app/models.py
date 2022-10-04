65b769aa0f8cdb045d782b237cc88ce889cfac38
Arsen Nuramatov <anuramat@pm.me>
2022-10-04 03:25:38 +0300

    semi-final models for this sprint, other minor changes

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Text, JSON
 from sqlalchemy.orm import relationship
-from .database import Base
+from database import Base
 
 
 class Paper(Base):
@@ -13,8 +13,6 @@ class Paper(Base):
     venue_id = Column(String(24), ForeignKey("venues.id", ondelete="CASCADE"))
     venue = relationship("Venue")
 
-    doc_type = Column(String(50))
-    # how will we update this? trigger on every new paper?
     citation_number = Column(Integer)
     year = Column(Integer)
     page_start = Column(Integer)
@@ -44,38 +42,48 @@ class Venue(Base):
 
 class Abstract(Base):
     __tablename__ = "abstracts"
-    paper_id = Column(String(24), ForeignKey("papers.id", ondelete="CASCADE"))
+    paper_id = Column(
+        String(24), ForeignKey("papers.id", ondelete="CASCADE"), primary_key=True
+    )
     abstract = Column(Text)
     indexed = Column(JSON)
 
 
 class PaperFieldOfStudy(Base):
     __tablename__ = "paper_fields_of_study"
-    paper_id = Column(String(24), ForeignKey("papers.id", ondelete="CASCADE"))
-    field_of_study = Column(String(50))
+    id = Column(Integer, primary_key=True)
+
+    paper_id = Column(
+        String(24), ForeignKey("papers.id", ondelete="CASCADE"), nullable=False
+    )
+    field_of_study = Column(String(50), nullable=False)
 
 
 class PaperKeyword(Base):
     __tablename__ = "paper_keywords"
-    paper_id = Column(String(24), ForeignKey("papers.id", ondelete="CASCADE"))
-    keyword = String(24)
+    id = Column(Integer, primary_key=True)
+
+    paper_id = Column(
+        String(24), ForeignKey("papers.id", ondelete="CASCADE"), nullable=False
+    )
+    keyword = Column(String(24), nullable=False)
 
 
 class References(Base):
     __tablename__ = "references"
-    paper_id = Column(String(24), ForeignKey("papers.id", ondelete="CASCADE"))
-    referenced_id = Column(String(24), ForeignKey("papers.id"))
+    id = Column(Integer, primary_key=True)
+
+    paper_id = Column(
+        String(24), ForeignKey("papers.id", ondelete="CASCADE"), nullable=False
+    )
+    referenced_id = Column(String(24), ForeignKey("papers.id"), nullable=False)
 
 
 class ExternalLink(Base):
     __tablename__ = "external_links"
-    paper_id = Column(String(24), ForeignKey("papers.id", ondelete="CASCADE"))
-    link = Column(String(200))
+    id = Column(Integer, primary_key=True)
 
-
-# class User(Base):
-#     id
-#     username
-#     author_id
-#     password_hash
-#     password_salt
+    paper_id = Column(
+        String(24), ForeignKey("papers.id", ondelete="CASCADE"), nullable=False
+    )
+    link = Column(String(200), nullable=False)
