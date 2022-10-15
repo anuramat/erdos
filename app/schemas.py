f65fbbd39df0fc7aa110355ca3c80bb64323c739
Arsen Nuramatov <anuramat@pm.me>
2022-10-15 12:17:19 +0300

    schemas refactored

--- app/schemas.py
+++ app/schemas.py
@@ -1,29 +1,6 @@
 from pydantic import BaseModel, constr
 
 
-class BasePaper(BaseModel):
-    __tablename__ = "papers"
-    id: constr(max_length=24)
-    author_id: constr(max_length=24)
-    venue_id: constr(max_length=24)
-
-    citation_number: int
-    year: int
-    page_start: int
-    page_end: int
-    publisher: constr(max_length=50)
-    issue: int
-    volume: int
-    language: constr(max_length=3)
-    issn: constr(max_length=9)
-    isbn: constr(max_length=30)
-    doi: constr(max_length=50)
-    pdf_url: constr(max_length=200)
-
-    class Config:
-        orm_mode = True
-
-
 class UpdatePaper(BaseModel):
     __tablename__ = "papers"
     author_id: constr(max_length=24) | None
@@ -44,3 +21,7 @@ class UpdatePaper(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+class BasePaper(UpdatePaper):
+    id: constr(max_length=24)
