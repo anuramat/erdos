58ebd2014a8c3cd3f1bd1be74bb5c0c542f6f3b8
Arsen Nuramatov <anuramat@pm.me>
2022-10-24 01:25:59 +0300

    error handling with auth

--- frontend/src/routes/search.js
+++ frontend/src/routes/search.js
@@ -2,8 +2,8 @@ import 'papers.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Papers, Tabs } from 'components/papers';
 import { Post } from 'components/post';
-import { useSearchParams, createSearchParams } from "react-router-dom";
-import { StrictMode, useEffect, useState } from 'react';
+import { useSearchParams } from "react-router-dom";
+import { useEffect, useState } from 'react';
 import queryString from 'query-string';
 import { useForm } from 'react-hook-form';
 
@@ -12,18 +12,36 @@ export const Search = () => {
     const [searchParams, setSearchParams] = useSearchParams();
     const { register, handleSubmit } = useForm({ defaultValues: queryString.parse(window.location.search) });
 
-    const LoadingError = () => {
+    const LoadingError = (e) => {
+        let desc = ""
+        switch (e) {
+            case 401:
+                desc = "not authenticated";
+                break;
+            default:
+                desc = "whoops";
+        }
         return (
-            <div className="content-container mt-5">whoops</div>
+            <div className="content-container mt-5">{desc}</div>
         )
     }
 
+    const searchResponseHandler = (response) => {
+        if (response.status === 401) {
+            throw 401;
+        }
+        return response
+    }
+
+    const root = window.location.protocol + "//" + window.location.hostname + ":8080/";
+
     const searchHandler = (form) => {
-        // TODO remove hardcode from url string
         setSearchParams(form)
-        fetch('http://localhost:8080/search?' + queryString.stringify(form), { method: "GET" })
-            .then(response => response.json()).then(papers =>
-                setPosts(papers.map((paper) => Post({ paper: paper })))).catch(() => setPosts(LoadingError()));
+        fetch(root + 'search?' + queryString.stringify(form), { method: "GET" })
+            .then(response => searchResponseHandler(response))
+            .then(response => response.json())
+            .then(papers => setPosts(papers.map(paper => Post({ paper: paper }))))
+            .catch((e) => setPosts(LoadingError(e)));
     }
 
     useEffect(() => {
