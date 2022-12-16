0d0e959847ac95aa110eaaf0a754d5075831ec12
Arsen Nuramatov <anuramat@pm.me>
2022-12-16 21:44:25 +0600

    xdxd

--- backend/app/dataloader.py
+++ backend/app/dataloader.py
@@ -1,6 +1,14 @@
 import csv
 import psycopg2
 from app.logic.clusters import get_cluster, get_tag
+from psycopg2 import errors
+
+
+def intx(s: str, default) -> int:
+    try:
+        return int(float(s))
+    except ValueError:
+        return default
 
 
 def start():
@@ -13,50 +21,58 @@ def start():
         for row in datareader:
 
             cur = con.cursor()
+            try:
+                # not always there
+                abs = row[19]
+                doi = row[16]
 
-            # not always there
-            abs = row[19]
-            doi = row[16]
+                authors = eval(row[8])
+                lang = row[7]
+                n_cit = intx(row[6], 0)
+                year = intx(row[4], None)
+                title = row[2]
+                pid = row[1]
 
-            authors = eval(row[8])
-            lang = row[7]
-            n_cit = int(float(row[6]))
-            year = int(float(row[4]))
-            title = row[2]
-            pid = row[1]
+                cluster = get_cluster(abs)
+                tag = get_tag(cluster)
 
-            cluster = get_cluster(abs)
-            tag = get_tag(cluster)
-
-            cur.execute(
-                "INSERT INTO papers (id, year, citation_number, language, doi, tag, cluster) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
-                (pid, year, n_cit, lang, doi, tag, cluster),
-            )
-
-            cur.execute(
-                "INSERT INTO abstracts (paper_id, text, title) VALUES (%s, %s, %s)",
-                (pid, abs, title),
-            )
-
-            for author in authors:
-                # check if author exists
-                aid = author["id"]
-                name = author["name"]
-                cur.execute("SELECT id FROM authors WHERE id = %s", (aid))
-                author_exists = bool(cur.fetchone())
-                if not author_exists:
-                    # create author if it doesn't exist
-                    cur.execute(
-                        "INSERT INTO authors (id, name) VALUES (%s, %s)", (aid, name)
-                    )
-                # update ratings
-                cur.execute("UPDATE authors SET rating = rating + %s", (n_cit,))
-
-                # add papers_authors entry
                 cur.execute(
-                    "INSERT INTO paper_authors (paper_id, author_id) VALUES (%s, %s)",
-                    (pid, aid),
+                    "INSERT INTO papers (id, year, citation_number, language, doi, tag, cluster) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                    (pid, year, n_cit, lang, tag, cluster),
                 )
+
+                cur.execute(
+                    "INSERT INTO abstracts (paper_id, text, title) VALUES (%s, %s, %s)",
+                    (pid, abs, title),
+                )
+
+                for author in authors:
+                    # check if author exists
+                    try:
+                        aid = author["_id"]
+                        name = author["name"]
+                    except KeyError:
+                        continue
+                    cur.execute("SELECT id FROM authors WHERE id = %s", (aid,))
+                    author_exists = bool(cur.fetchone())
+                    if not author_exists:
+                        # create author if it doesn't exist
+                        cur.execute(
+                            "INSERT INTO authors (id, name) VALUES (%s, %s)",
+                            (aid, name),
+                        )
+                    # update ratings
+                    cur.execute("UPDATE authors SET rating = rating + %s", (n_cit,))
+
+                    # add papers_authors entry
+                    cur.execute(
+                        "INSERT INTO paper_authors (paper_id, author_id) VALUES (%s, %s)",
+                        (pid, aid),
+                    )
+            except errors.UniqueViolation:
+                con.rollback()
+                cur.close()
+                continue
 
             con.commit()
             cur.close()
